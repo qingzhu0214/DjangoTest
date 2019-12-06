560d7c3955460504deb3080157140c367c97b26c
qingzhu0214 <qingzhu0214@github.com>
2019-12-06 20:55:50 +0800

    1

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from login import models
 # Create your views here.
 
+user_list = []
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        models.UserInfo.objects.create(user=username, pwd = password)
+
+    user_list = models.UserInfo.objects.all()
+    return render(request, 'index.html', {'data':user_list})
